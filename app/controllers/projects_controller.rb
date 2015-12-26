51eb2e23a8fd476dd54b63ab7a35debe6c1465f1
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-12-26 17:23:29 +0100

    new view and table projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,4 +7,21 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      flash[:notice] = "Project has been created."
+      redirect_to @project
+    else
+      # nothing, yet
+    end
+  end
 end
+
+
+private
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
--- db/migrate/20151226115447_create_projects.rb
+++ db/migrate/20151226115447_create_projects.rb
@@ -1,10 +1,14 @@
 class CreateProjects < ActiveRecord::Migration
-  def change
+  def up
     create_table :projects do |t|
       t.string :name
       t.string :description
 
-      t.timestamps null: false
+      t.timestamps
     end
+  end
+
+  def down
+    drop_table :projects
   end
 end
