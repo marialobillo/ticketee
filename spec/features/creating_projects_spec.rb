6445989e2b675e893632142e5656701e720a4528
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-12-27 17:34:39 +0100

    application helper

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 feature 'Creating Projects' do
 
   scenario "can create a project" do
-    visit '/'
+  #  visit '/'
 
-    click_link 'New Project'
+#    click_link 'New Project'
 
-    fill_in 'Name', with: 'Atom'
-    fill_in 'Description', with: 'A text-editor for OS X'
-    click_button 'Create Project'
+#    fill_in 'Name', with: 'Atom'
+#    fill_in 'Description', with: 'A text-editor for OS X'
+#    click_button 'Create Project'
 
-    expect(page).to have_content('Project has been created.')
+#    expect(page).to have_content('Project has been created.')
 
     project = Project.where(name: "Atom 1").first
     expect(page.current_url).to eql(project_url(project))
---
+++ spec/helpers/application_helper.rb
@@ -0,0 +1,9 @@
+module APplicationHelper
+  def title(*parts)
+    unless parts.empty?
+      content_for :title do
+        (parts << "Ticketee").join(" - ")
+      end
+    end
+  end
+end
