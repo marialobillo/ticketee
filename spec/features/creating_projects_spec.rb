150810a5b2cea01ee7ecebe27da38aa5a578f36b
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-12-27 18:42:26 +0100

    validations on project

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,22 +1,31 @@
 require 'spec_helper'
 
 feature 'Creating Projects' do
+  before do
+    visit '/'
+
+    click_link 'New Project'
+  end
 
   scenario "can create a project" do
-  #  visit '/'
+    fill_in 'Name', with: 'Atom 1'
+    fill_in 'Description', with: 'A text-editor for OS X'
+    click_button 'Create Project'
 
-#    click_link 'New Project'
-
-#    fill_in 'Name', with: 'Atom'
-#    fill_in 'Description', with: 'A text-editor for OS X'
-#    click_button 'Create Project'
-
-#    expect(page).to have_content('Project has been created.')
+    expect(page).to have_content('Project has been created.')
 
     project = Project.where(name: "Atom 1").first
+
     expect(page.current_url).to eql(project_url(project))
 
     title = "Atom 1 - Projects - Ticketee"
     expect(page).to have_title(title)
+  end
+
+  scenario "can not create a project without a name" do
+    click_button 'Create Project'
+
+    expect(page).to have_content("Project has not been created.")
+    expect(page).to have_content("Name can't be blank.")
   end
 end
