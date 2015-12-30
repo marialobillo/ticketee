b7e8f2147b0085b7f27b4457882f8d81e89eb2d7
maria_lobillo <maria.lobillo.santos@gmail.com>
2015-12-30 18:59:25 +0100

    redirect to home if not exists the project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
 
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
   def index
     @projects = Project.all
   end
@@ -9,11 +11,11 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    @project = Project.find(params[:id])
+  #  @project = Project.find(params[:id])
   end
 
   def edit
-    @project = Project.find(params[:id])
+  #  @project = Project.find(params[:id])
   end
 
   def create
@@ -30,7 +32,7 @@ class ProjectsController < ApplicationController
   end
 
   def update
-    @project = Project.find(params[:id])
+  #  @project = Project.find(params[:id])
     if @project.update(project_params)
       flash[:notice] = "Project has been updated."
       redirect_to @project
@@ -41,7 +43,7 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project = Project.find(params[:id])
+  #  @project = Project.find(params[:id])
     @project.destroy
 
     flash[:notice] = "Project has been destroyed."
@@ -52,5 +54,13 @@ class ProjectsController < ApplicationController
   private
     def project_params
       params.require(:project).permit(:name, :description)
+    end
+
+    def set_project
+      @project = Project.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      flash[:alert] = "The project you were looking" +
+                      " for could not be found."
+      redirect_to projects_path
     end
 end
