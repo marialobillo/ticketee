eb6eac03fcbbb63117742b6b1501f8cd37186249
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-01-03 12:58:17 +0100

    form for tickets and create method

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -6,7 +6,26 @@ class TicketsController < ApplicationController
     @ticket = @project.tickets.build
   end
 
+  def create
+    @ticket = @project.tickets.build(ticket_params)
+    if @ticket.save
+      flash[:notice] = "Ticket has been created."
+      redirect_to [@project, @ticket]
+    else
+      flash[:notice] = "Ticket has not been created."
+      render 'new'
+    end
+  end
+
 
   private
-  
+
+    def set_project
+      @project = Project.find(params[:project_id])
+    end
+
+    def ticket_params
+      params.require(:ticket).permit(:title, :description)
+    end
+
 end
