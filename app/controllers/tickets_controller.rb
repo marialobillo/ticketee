56c3d13f80201f1bbc2763271873115cfc0f3354
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-01-03 13:02:43 +0100

    set ticket

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,6 +1,7 @@
 class TicketsController < ApplicationController
 
   before_action :set_project
+  before_action :set_ticket, only: [:show, :edit, :update, :destroy]
 
   def new
     @ticket = @project.tickets.build
@@ -26,6 +27,10 @@ class TicketsController < ApplicationController
 
     def ticket_params
       params.require(:ticket).permit(:title, :description)
+    end
+
+    def set_ticket
+      @ticket = @project.tickets.find(params[:id])
     end
 
 end
