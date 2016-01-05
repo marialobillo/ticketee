339883995f612f605a96cac5aa18a423ef3266fe
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-01-05 13:22:01 +0100

    ticket factory

---
+++ spec/features/viewing_tickets_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+feature "Viewing tickets" do
+
+  before do
+    atom_2 = FactoryGirl.create(:project, name: "Atom 2")
+
+    FactoryGirl.create(:ticket,
+          project: atom_2,
+          title: "Make it shiny!",
+          description: "Gradients! Starbursts! Oh my!")
+
+    internet_explorer = FactoryGirl.create(:project,
+                                          name: "Internet Explorer")
+
+    FactoryGirl.create(:ticket,
+          project: internet_explorer,
+          title: "Standards compliance",
+          description: "Isn't a joje.")
+
+    visit '/'
+  end
+
+  scenario "Viewing tickets for a given project" do
+    click_link "Atom 2"
+
+    expect(page).to have_content("Make it shiny!")
+    expect(page).to_not have_content("Standards compliance")
+
+    click_link "Make it shiny!"
+    whithin('#ticket h2') do
+      expect(page).to have_content("Make it shiny!")
+    end
+
+    expect(page).to have_content("Gradients! Starbursts! Oh my!")
+  end
+end
