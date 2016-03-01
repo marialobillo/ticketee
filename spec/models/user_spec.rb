83809f3967138529949c1bbaf4fa3c9a740ee42a
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-03-01 15:56:44 +0100

    user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  describe 'passwords' do
+    it 'needs a passwors and confirmation to save' to
+      u = User.new(name: 'steve')
+
+      u.save
+      expect(u).to_not be_valid
+
+      u.password = 'password'
+      u.password_confirmation = ''
+      u.save
+      expect(u).to_not be_valid
+
+      u.password_confirmation = 'password'
+      u.save
+      expect(u).to be_valid
+    end
+
+    it "needs password and confirmation to match" do
+      u = User.create(
+          name: 'steve',
+          password: 'hunter2',
+          password_confirmation: 'hunter'
+      )
+      expect(u).to_not be_valid
+    end
+  end
+
+  describe "authentication" do
+    let(:user) { User.create(
+          name: 'steve',
+          password: 'hunter2',
+          password_confirmation: 'hunter2'
+      ) }
+
+    it "authentication with a correct password" do
+      expect(user.authentication("hunter2")).to be
+    end
+
+    it "does not authenticate with an incorrect password" do
+      expect(user.authenticate("hunter1")).to_not be 
+    end
+  end
 end
