3645f4d166a85fe3b7e7c00852a472c22d6bcd83
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-03-01 17:23:50 +0100

    users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/show'
+
   root "projects#index"
 
   resources :projects do
     resources :tickets
   end
+
+  resources :users
 end
