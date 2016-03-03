4618eae46d9a34e5a6f4c0108556407777a3d572
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-03-03 13:00:00 +0100

    Editing for user, spec

--- spec/features/user_profile_spec.rb
+++ spec/features/user_profile_spec.rb
@@ -10,3 +10,17 @@ feature "Profile page" do
     expect(page).to have_content(user.email)
   end
 end
+
+feature "Editing User" do
+  scenario "Updating a project" do
+    user = FactoryGirl.create(:user)
+
+    visit user_path(user)
+    click_link "Edit Profile"
+
+    fill_in "Username", with: "new_username"
+    click_button "Update Profile"
+
+    expect(page).to have_content("Profile has been updated.")
+  end
+end
