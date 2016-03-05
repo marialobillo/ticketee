e4ae399f1545f6c809801db824b37d4dbe5c09cd
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-03-05 12:19:57 +0100

    something  I forgot

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,12 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[signin][:password])
       session[:user_id] = user.id
       flash[:notice] = "Signed in succesfully."
+
+      redirect_to root_url
+    else
+      flash[:error] = "Sorry."
+      render :new
+    end
   end
 
 end
