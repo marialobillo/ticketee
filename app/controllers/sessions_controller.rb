513121ce2a8616cc3b7392d7f9d40c02a97a890f
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-03-05 12:14:26 +0100

    create a new session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,12 @@
 class SessionsController < ApplicationController
+
+
+  def create
+    user = User.where(:name => params[:signin][:name]).first
+
+    if user && user.authenticate(params[signin][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Signed in succesfully."
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root "projects#index"
   get "/signin", to: "sessions#new"
+  post "/signin", to: "sessions#create"
 
   resources :projects do
     resources :tickets
