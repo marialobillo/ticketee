ad7880bef61e1440fffe130210d7c2da4bd63195
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-03-13 20:18:35 +0100

    removing errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
     helper_method :require_signin!
 
     def current_user
-      @current_user ||= User.find(session[:user_id]) if sesssion[:user_id]
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
     helper_method :current_user
 end
