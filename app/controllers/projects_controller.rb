37ff7c9ea93092dbf642a050da3c5a69b4bd10f0
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-03-14 10:37:47 +0100

    auth admin

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -63,4 +63,13 @@ class ProjectsController < ApplicationController
                       " for could not be found."
       redirect_to projects_path
     end
+
+    def authorize_admin!
+      require_signin!
+
+      unless current_user.admin?
+        flash[:alert] = "You must be an admin to do that"
+        redirect_to root_path
+      end
+    end
 end
