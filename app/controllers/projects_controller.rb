b86be2d4043f2b913e1e52d76d70c87681d4ef34
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-03-15 13:13:44 +0100

    authorize admin

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,6 @@
 class ProjectsController < ApplicationController
 
+  before_action :authorize_admin!, except: [:index, :show]
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   def index
