8076a0f199836228a39a983d9c533f929ab30213
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-03-16 18:17:20 +0100

    action

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -8,6 +8,14 @@ RSpec.describe ProjectsController, type: :controller do
       sign_in(user)
     end
 
+    {
+      new: :get,
+      create: :post,
+      edit: :get,
+      update: :put,
+      destroy: :delete
+    }.each do |action, method|
+
     it "cannot access the new action" do
       get :new
 
@@ -21,5 +29,12 @@ RSpec.describe ProjectsController, type: :controller do
     expect(response).to redirect_to(project_path)
     message = "The project you were looking for could not be found."
     expect(flash[:alert]).to eql(message)
+  end
+
+  it "cannot access the #{action} action" do
+    sign_in(user)
+    send(method, action, :id => FactoryGirl.create(:project))
+    expect(response).to redirect_to(root_path)
+    expect(flash[:alert]).to eql("You must be an admin to do that.")
   end
 end
