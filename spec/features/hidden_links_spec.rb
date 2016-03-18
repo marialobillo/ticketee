e9e6f67ffa47762d32910b0e0fcd00282713726a
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-03-18 08:45:11 +0100

    hidden links spec

---
+++ spec/features/hidden_links_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+feature "hidden links" do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:admin) { FactoryGirl.create(:admin_user) }
+
+  context "anonymous users" do
+    scenario "cannot see the New Project link" do
+      visit '/'
+      assert_no_link_for "New Project"
+    end
+  end
+
+  context "regular users" do
+    before { sign_in_as!(user) }
+    scenario "cannot see the New Project link" do
+      visit '/'
+      assert_no_link_for "New Project"
+    end
+  end
+
+  context "admin users" do
+    before { sign_in_as!(admin) }
+    scenario "can see the New Project link" do
+      visit '/'
+      assert_link_for "New Project"
+    end
+  end
+end
