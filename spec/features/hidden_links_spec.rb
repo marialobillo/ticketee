05eae8f5d4e0b4658eae6529dcb55ceb1f6ea073
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-03-21 16:45:03 +0100

    scenario delete project

--- spec/features/hidden_links_spec.rb
+++ spec/features/hidden_links_spec.rb
@@ -35,6 +35,10 @@ feature "hidden links" do
       assert_no_link_for "Edit Project"
     end
 
-    
+    scenario "cannot see the Delete Project link" do
+      visit project_path(project)
+      assert_no_link_for "Delete Project"
+    end
+
   end
 end
