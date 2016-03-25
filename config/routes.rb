49ec866dfb2d9173a267fbcdced38cea96830fea
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-03-25 12:23:55 +0100

    admin users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+  get 'users/index'
+  end
+
   root "projects#index"
   get "/signin", to: "sessions#new"
   post "/signin", to: "sessions#create"
