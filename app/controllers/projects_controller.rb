84070a219a1a6caa75a7de5d096fdf4303cb2753
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-03-28 17:19:14 +0200

    removing authorize_admin

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -65,12 +65,12 @@ class ProjectsController < ApplicationController
       redirect_to projects_path
     end
 
-    def authorize_admin!
-      require_signin!
+  #  def authorize_admin!
+  #    require_signin!
 
-      unless current_user.admin?
-        flash[:alert] = "You must be an admin to do that"
-        redirect_to root_path
-      end
-    end
+  #    unless current_user.admin?
+  #      flash[:alert] = "You must be an admin to do that"
+  #      redirect_to root_path
+  #    end
+  #  end
 end
