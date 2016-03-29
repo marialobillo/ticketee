4aa41cdb01316a63276fc941220a85cfda05767b
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-03-29 13:16:26 +0200

    before do creating users

--- spec/features/creating_users_spec.rb
+++ spec/features/creating_users_spec.rb
@@ -3,4 +3,12 @@ require 'spec_helper'
 feature "Creating Users" do
   let!(:admin) { FactoryGirl.create(:admin_user) }
 
+  before do
+    sign_in_as!(admin)
+    visit '/'
+    click_link "Admin"
+    click_link "Users"
+    click_link "New User"
+  end
+
 end
