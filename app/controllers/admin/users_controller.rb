fd062433b866844821d45177a1928beb566e8130
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-04-02 11:53:47 +0200

    user_params definition

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -23,5 +23,10 @@ class Admin::UsersController < Admin::BaseController
     end
   end
 
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :password, :password_confirmation)
+    end
 
 end
