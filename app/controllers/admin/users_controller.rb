24d6f03d95597c9134e7359e8dd309da8eadb393
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-04-02 11:53:00 +0200

    create definition for new admins

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,4 +8,20 @@ class Admin::UsersController < Admin::BaseController
   def new
     @user = User.new
   end
+
+  def create
+    params = user_params.dup
+    params[:password_confirmation] = params[:password]
+    @user = User.new(params)
+
+    if @user.save
+      flash[:notice] = "User has been created."
+      redirect_to admin_users_path
+    else
+      flash.now[:alert] = "User has not been created."
+      render :action => "new"
+    end
+  end
+
+
 end
