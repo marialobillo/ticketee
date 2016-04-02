3ce054e9546c472409859c4be33d3506e86d080e
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-04-02 12:01:32 +0200

    scenario for creating an admin user

--- spec/features/admin/creating_users_spec.rb
+++ spec/features/admin/creating_users_spec.rb
@@ -2,4 +2,15 @@ require 'spec_helper'
 
 feature "Creating Admin Users" do
 
+  scenario "Creating an admin user" do
+    fill_in "Email", with: "admin@example.com"
+    fill_in "Password", with: "password"
+    check "Is an admin?"
+    click_button "Create User"
+    expect(page).to have_content("User has been created.")
+    within("#users") do
+      expect(page).to have_content("admin@example.com (Admin)")
+    end
+  end
+
 end
