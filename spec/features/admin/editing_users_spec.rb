587ccd24f78e3003ce8bcc89f91155b0062e6279
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-04-05 13:30:56 +0200

    scenario for updating a users details

--- spec/features/admin/editing_users_spec.rb
+++ spec/features/admin/editing_users_spec.rb
@@ -13,4 +13,16 @@ feature 'Editing a user' do
      click_link user.email
      click_link "Edit User"
    end
+
+   scenario "Updating a user's details" do
+     fill_in "Email", with: "newguy@example.com"
+     click_button "Update User"
+
+     expec(page).to have_content("User has been updated.")
+
+     within("#users") do
+       expect(page).to have_content("newguy@example.com")
+       expect(page).to_not have_content(user.email)
+     end
+   end
 end
