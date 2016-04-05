5a21664802f76aec3b7254b769e4c36418c61028
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-04-05 13:28:18 +0200

    before do ...admin/user

--- spec/features/admin/editing_users_spec.rb
+++ spec/features/admin/editing_users_spec.rb
@@ -2,5 +2,15 @@ require 'spec_helper'
 
 feature 'Editing a user' do
 
-   
+   let!(:admin_user) { FactoryGirl.create(:admin_user) }
+   let!(:user) { FactoryGirl.create(:user) }
+
+   before do
+     sign_in_as!(admin_user)
+     visit '/'
+     click_link "Admin"
+     click_link "Users"
+     click_link user.email
+     click_link "Edit User"
+   end
 end
