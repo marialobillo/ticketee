dfee0fcc8a503340eb1c4907a73ee4e6bf9799b1
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-04-06 16:52:03 +0200

    spec for requires an email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -42,5 +42,15 @@ describe User do
     it "does not authenticate with an incorrect password" do
       expect(user.authenticate("hunter1")).to_not be
     end
+
+    it "requires an email" do
+      u = User.new(name: "steve",
+                  password: "hunter2",
+                  password_confirmation: "hunter2")
+      u.save
+      expect(u).to_not be_valid
+
+      
+    end
   end
 end
