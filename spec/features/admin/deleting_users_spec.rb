08c0ef395e8daf3f568ea7540fe8d8dff474739f
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-04-15 10:06:33 +0200

    before do for deleting users spec

--- spec/features/admin/deleting_users_spec.rb
+++ spec/features/admin/deleting_users_spec.rb
@@ -1,1 +1,14 @@
 require 'spec_helper'
+
+feature 'Deleting users' do
+  let!(:admin_user) { FactoryGirl.create(:admin_user) }
+  let!(:user) { FactoryGirl.create(:user) }
+
+  before do
+    sign_in_as! (admin_user)
+    visit '/'
+
+    click_link 'Admin'
+    click_link 'Users'
+  end
+end
