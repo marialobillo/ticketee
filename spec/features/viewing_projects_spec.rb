b7b08ce4eb13b1a9fd8c0b81832b1b4f58e2e0db
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-04-19 09:56:15 +0200

    viewing projects spec

--- spec/features/viewing_projects_spec.rb
+++ spec/features/viewing_projects_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 feature "Viewing projects" do
+  let!(:user) { FactoryGirl.create(:user) }
+  let!(:project) { FactoryGirl.create(:project) }
   scenario "Listing all projects" do
     project = FactoryGirl.create(:project, name: "Atom 1")
     visit '/'
