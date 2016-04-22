11dcb740d66e9ca7a51eae2b5831cd7c7cd360e3
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-04-22 10:06:21 +0200

    permissions on project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,9 @@ class Project < ActiveRecord::Base
 
   has_many :tickets, dependent: :delete_all
   validates :name, presence: true
+
+  has_many :permissions, as: :thing
+  scope :viewable_by, ->(user) do
+    joins(:permissions).where(permissions: { action: "view", user_id: user.id })
+  end
 end
