93c668d1921c6aacc945b6a61543c7f368eb8068
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-04-25 10:11:55 +0200

    refactoring

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,7 +4,7 @@ class Project < ActiveRecord::Base
   validates :name, presence: true
 
   has_many :permissions, as: :thing
-  scope :viewable_by, ->(user) do
-    joins(:permissions).where(permissions: { action: "view", user_id: user.id })
+  scope :for, ->(user) do
+    user.admin? ? Project.all : Project.viewable_by(user)
   end
 end
