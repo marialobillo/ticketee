b14548fbc2837a0a80e1d8876cc0487d5ad869c1
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-04-26 09:22:36 +0200

    user id from session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id 
       flash[:notice] = "You have signed up succesfully."
       redirect_to projects_path
     else
