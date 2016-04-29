a744f5b17908423155aa8237f1f843a3ede4e7d4
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-04-29 08:45:18 +0200

    auth to create

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -3,7 +3,7 @@ class TicketsController < ApplicationController
   before_action :require_signin!
   before_action :set_project
   before_action :set_ticket, only: [:show, :edit, :update, :destroy]
-  before_action :require_signin!
+  before_action :authorize_create!, only: [:new, :create]
 
   def new
     @ticket = @project.tickets.build
