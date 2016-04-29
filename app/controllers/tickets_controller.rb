a7361220e2b8a227c4e285166190c8cf16438695
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-04-29 08:44:29 +0200

    for tickets require signin

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,5 +1,6 @@
 class TicketsController < ApplicationController
 
+  before_action :require_signin!
   before_action :set_project
   before_action :set_ticket, only: [:show, :edit, :update, :destroy]
   before_action :require_signin!
