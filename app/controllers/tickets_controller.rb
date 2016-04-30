b327a093d6a389615f42c2b6283018d6c04e6013
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-04-30 08:12:28 +0200

    current user admin can create tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -49,7 +49,10 @@ class TicketsController < ApplicationController
     end
 
     def authorize_create!
-
+      if !current_user.admin? && cannot?("create tickets".to_sym, @project)
+        flash[:alert] = "You cannot create tickets on this project."
+        redirect_to @project
+      end
     end
 
 end
