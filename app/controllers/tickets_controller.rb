fba61d3b4dccce915697de8df5e358b054fa3f24
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-05-03 12:50:39 +0200

    let edit tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -57,7 +57,10 @@ class TicketsController < ApplicationController
     end
 
     def authorize_update!
-
+      if !current_user.admin? && cannot?("edit tickets".to_sym, @project)
+        flash[:alert] = "You cannot edit tickets on this project."
+        redirect_to @project
+      end
     end
 
 end
