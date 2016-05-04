578fe31e46c5f93dbcda16ec8bac09f99b0cc658
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-05-04 09:21:30 +0200

    deleting tickets

--- spec/features/deleting_tickets_spec.rb
+++ spec/features/deleting_tickets_spec.rb
@@ -7,8 +7,10 @@ feature 'Deleting tickets' do
     FactoryGirl.create(:ticket, project: project, user: user) }
 
   before do
+    define_permission!(user, "view", project)
+    define_permission!(user, "delete tickets", project)
     sign_in_as!(user)
-    
+
     visit '/'
     click_link project.name
     click_link ticket.title
