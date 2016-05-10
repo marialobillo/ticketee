4053275c22fa7603482ad0ef4df255c2673cedee
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-05-10 09:24:20 +0200

    FactoryGirl create: admin, user, project, ticket

--- spec/features/admin/assigning_permissions_spec.rb
+++ spec/features/admin/assigning_permissions_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 feature "Assigning permissions" do
-  
+  let!(:admin) { FactoryGirl.create(:admin_user) }
+  let!(:user) { FactoryGirl.create(:user) }
+  let!(:project) { FactoryGirl.create(:project) }
+  let!(:ticket) { FactoryGirl.create(:ticket, project: project, user: user) } 
 
 end
