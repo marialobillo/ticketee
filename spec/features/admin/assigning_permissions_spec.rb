42f8f216b50632df50465c58aa85b4f528c9d8e3
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-05-10 09:25:13 +0200

    before do

--- spec/features/admin/assigning_permissions_spec.rb
+++ spec/features/admin/assigning_permissions_spec.rb
@@ -4,6 +4,15 @@ feature "Assigning permissions" do
   let!(:admin) { FactoryGirl.create(:admin_user) }
   let!(:user) { FactoryGirl.create(:user) }
   let!(:project) { FactoryGirl.create(:project) }
-  let!(:ticket) { FactoryGirl.create(:ticket, project: project, user: user) } 
+  let!(:ticket) { FactoryGirl.create(:ticket, project: project, user: user) }
+
+  before do
+    sign_in_as!(admin)
+
+    click_link "Admin"
+    click_link "Users"
+    click_link user.email
+    click_link "Permissions"
+  end
 
 end
