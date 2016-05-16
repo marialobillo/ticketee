e692c1e380b7c0af9fb4a5415c503ed6b42936f6
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-05-16 08:40:51 +0200

    delete session on routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   get "/signin", to: "sessions#new"
   post "/signin", to: "sessions#create"
 
+  delete "/signout", to: "sessions#destroy", as: "signout"
+
   resources :projects do
     resources :tickets
   end
