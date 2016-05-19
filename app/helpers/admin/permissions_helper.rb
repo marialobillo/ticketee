e5384abae9b9841041ca2c552a570cb3153c345d
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-05-19 09:13:13 +0200

    definition permissions

--- app/helpers/admin/permissions_helper.rb
+++ app/helpers/admin/permissions_helper.rb
@@ -2,7 +2,9 @@ module Admin::PermissionsHelper
   def permissions
     {
         "view" => "View",
-        "create tickets" => "Create Tickets"
+        "create tickets" => "Create Tickets",
+        "edit tickets" => "Edit Tickets",
+        "delete tickets" => "Delete Tickets"
     }
   end
 
