49b3bc5aff7bb1bd92325c78765eaa9c09ec63b2
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-05-19 09:48:50 +0200

    user and project

--- spec/features/seed_spec.rb
+++ spec/features/seed_spec.rb
@@ -3,5 +3,7 @@ require 'spec_helper'
 feature "Seed Data" do
   scenario "The basics" do
     load Rails.root + "db/seeds.rb"
+    user = User.where(email: "admin@example.com").first!
+    project = Project.where(name: "Ticketee Beta").first!
   end
 end
