29aac8209e4df525556a351a8ddc8293af9f4ddd
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-05-20 09:09:47 +0200

    Project T. Beta

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,4 @@ admin_user = User.create(email: "admin@example.com",
                           password: "password",
                           password_confirmation: "password",
                           admin: true)
+Project.create(name: "Ticketee Beta")
