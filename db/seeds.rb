b410ad9aa7420f8927461c9734be41e3efb71122
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-05-20 09:08:44 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+admin_user = User.create(email: "admin@example.com",
+                          name: "admin",
+                          password: "password",
+                          password_confirmation: "password",
+                          admin: true)
