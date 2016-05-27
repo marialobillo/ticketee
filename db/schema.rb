73e0253f964ec309c68634d3261119cdd8a26e04
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-05-27 09:11:09 +0200

    run the migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160523101900) do
+ActiveRecord::Schema.define(version: 20160526172315) do
+
+  create_table "assets", force: :cascade do |t|
+    t.string   "asset"
+    t.integer  "ticket_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "permissions", force: :cascade do |t|
     t.integer  "user_id"
@@ -36,7 +43,6 @@ ActiveRecord::Schema.define(version: 20160523101900) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
-    t.string   "asset"
   end
 
   add_index "tickets", ["project_id"], name: "index_tickets_on_project_id"
