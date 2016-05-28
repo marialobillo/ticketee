7bddafb60c32f18bddb9d4838c949685e0b62289
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-05-28 13:33:07 +0200

    project and ticket

--- spec/controllers/files_controller_spec.rb
+++ spec/controllers/files_controller_spec.rb
@@ -4,4 +4,7 @@ RSpec.describe FilesController, type: :controller do
   let(:good_user) { FactoryGirl.create(:user) }
   let(:bad_user) { FactoryGirl.create(:user)}
 
+  let(:project) { FactoryGirl.create(:project) }
+  let(:ticket) { FactoryGirl.create(:ticket, project: project, user: good_user) }
+
 end
