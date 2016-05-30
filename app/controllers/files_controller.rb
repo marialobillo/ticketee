c5f8a7a5e3074e718c2217aec58deb13ada97c55
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-05-30 11:16:52 +0200

    show method

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -1,4 +1,10 @@
 class FilesController < ApplicationController
   before_filter :require_signin!
 
+  def show
+    asset = Asset.find(params[:id])
+    send_file asset.asset.path,
+        filename: asset.asset_identifier,
+        content_type: asset.content_type
+  end
 end
