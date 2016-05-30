76444f8c2f2c6f311f1b0a42c8ed057c0ffdd173
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-05-30 11:36:09 +0200

    assets table

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160526172315) do
+ActiveRecord::Schema.define(version: 20160530091855) do
 
   create_table "assets", force: :cascade do |t|
     t.string   "asset"
     t.integer  "ticket_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "content_type"
   end
 
   create_table "permissions", force: :cascade do |t|
