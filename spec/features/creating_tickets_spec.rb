7ba063958bd1091f1a10134d46c59aa512c44654
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-06-01 10:20:16 +0200

    removing somthing

--- spec/features/creating_tickets_spec.rb
+++ spec/features/creating_tickets_spec.rb
@@ -55,16 +55,15 @@ feature "Creating Tickets" do
     attach_file "File #2", Rails.root.join("spec/fixtures/spin.txt")
 
 
-    attach_file "File #3", Rails.root.join("spec/fixtures/gradient.txt")
+    #attach_file "File #3", Rails.root.join("spec/fixtures/gradient.txt")
 
     click_button "Create Ticket"
 
     expect(page).to have_content("Ticket has been created.")
 
-    within("#ticket .asset") do
+    within("#ticket .assets") do
       expect(page).to have_content("speed.txt")
       expect(page).to have_content("spin.txt")
-      expect(page).to have_content("gradient.txt")
     end
   end
 end
