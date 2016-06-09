896b4cabbcce9e3bcadbf03a47e150813647f7be
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-06-09 11:02:04 +0200

    let let let

--- spec/integration/creating_comments_spec.rb
+++ spec/integration/creating_comments_spec.rb
@@ -1,1 +1,7 @@
 require 'spec_helper'
+
+feature "Creating comments" do
+  let!(:user) { Factory(:confirmed_user) }
+  let!(:project) { Factory(:project) }
+  let!(:ticket) { Factory(:ticket, :project => project, :user => user) }
+end
