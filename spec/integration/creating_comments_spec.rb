c536af8713c824ef414e96ed0220f3666265561c
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-06-09 11:04:38 +0200

    before do

--- spec/integration/creating_comments_spec.rb
+++ spec/integration/creating_comments_spec.rb
@@ -4,4 +4,12 @@ feature "Creating comments" do
   let!(:user) { Factory(:confirmed_user) }
   let!(:project) { Factory(:project) }
   let!(:ticket) { Factory(:ticket, :project => project, :user => user) }
+
+  before do
+    define_permission!(user, "view", project)
+
+    sign_in_as!(user)
+    visit '/'
+    click_link project.name
+  end
 end
