74c908b932ebe635717632c6eb65a7727d70d823
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-06-11 12:00:41 +0200

    comments create method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,16 @@ class CommentsController < ApplicationController
 
   before_filter :authenticate_user!
   before_filter :find_ticket
+
+  def create
+    @comment = @ticket.comments.build(params[:comment])
+    @comment.user = current_user
+
+    if @comment.save
+      flash[:notice] = "Comment has been created."
+      redirect_to [@ticket.project, @ticket] <co id="ch10_v2_5_1" />
+    else
+      
+    end
+  end
 end
