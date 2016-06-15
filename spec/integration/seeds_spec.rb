583f5be88f33cce32ce14db619a7b0d2fa591aae
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-06-15 19:31:16 +0200

    Comments and create tickets...

--- spec/integration/seeds_spec.rb
+++ spec/integration/seeds_spec.rb
@@ -7,5 +7,13 @@ feature "Seed Data" do
     sign_in_as!(user)
     click_link "Ticketee Beta"
     click_link "New Ticket"
+    fill_in "Title", :with => "Comments with state"
+    fill_in "Description", :with => "Comments always have a state."
+    click_button "Create Ticket"
+    within("#comment_state_id") do
+      page.should have_content("New")
+      page.should have_content("Open")
+      page.should have_content("Closed")
+    end
   end
 end
