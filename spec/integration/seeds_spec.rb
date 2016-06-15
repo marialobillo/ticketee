a2b6f81cbd0330db4fe31763bdd29dd0a8752262
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-06-15 19:26:58 +0200

    click links

--- spec/integration/seeds_spec.rb
+++ spec/integration/seeds_spec.rb
@@ -3,6 +3,9 @@ require 'spec_helper'
 feature "Seed Data" do
   scenario "The basics" do
     load Rails.root + "db/seeds.rb"
-    
+    user = User.find_by_email!("admin@ticketee.com")
+    sign_in_as!(user)
+    click_link "Ticketee Beta"
+    click_link "New Ticket"
   end
 end
