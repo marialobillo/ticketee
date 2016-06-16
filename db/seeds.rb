2b8d43d55460b3e38776bc1ddbb793f1be13a8f6
maria_lobillo <maria.lobillo.santos@gmail.com>
2016-06-16 08:08:40 +0200

    open state, seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,4 +16,6 @@ State.create(:name => "New",
               :background => "#85FF00",
               :color => "white")
 
-              
+State.create(:name => "Open",
+              :background => "#00CFFD",
+              :color => "white")
