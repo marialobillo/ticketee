885197db1f63b208d258fd5cf04f296af9a9a7e6
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-06-20 17:23:30 +0200

    bundle exec

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160612103226) do
+ActiveRecord::Schema.define(version: 20160620152142) do
 
   create_table "assets", force: :cascade do |t|
     t.string   "asset"
@@ -25,9 +25,10 @@ ActiveRecord::Schema.define(version: 20160612103226) do
     t.text     "text"
     t.integer  "ticket_id"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
     t.integer  "state_id"
+    t.integer  "previous_state_id"
   end
 
   create_table "permissions", force: :cascade do |t|
