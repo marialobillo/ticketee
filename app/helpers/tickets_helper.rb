4f6861e751021bf9c2230c7404c8226649232695
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-06-21 20:17:39 +0200

    render

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -2,6 +2,10 @@ module TicketsHelper
   def state_for(comment)
     content_tag(:div, :class => "states") do
       if previous_state && comment.state != previous_state
+        "#{render previous_state} &rarr; #{render comment.state}"
+      else
+        render(comment.state)
+      end
   end
 
 end
