f8698eec5040cc0918eb845d39adb431f091b4d6
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-06-23 08:27:27 +0200

    click links

--- spec/integration/creating_states_spec.rb
+++ spec/integration/creating_states_spec.rb
@@ -4,5 +4,10 @@ feature 'Creating states' do
   before do
     sign_in_as!(Factory(:admin_user))
   end
-  
+
+  scenario "Creating a state" do
+    click_link "Admin"
+    click_link "States"
+    click_link "New State"
+  end
 end
