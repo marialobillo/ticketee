c9e88d900243298a680e041c7cefb39d044b7c8c
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-06-28 17:59:03 +0200

    create method in states

--- app/controllers/admin/states_controller.rb
+++ app/controllers/admin/states_controller.rb
@@ -6,6 +6,16 @@ class Admin::StatesController < ApplicationController
 
   def new
     @state = State.new
-  end 
+  end
 
+  def create
+    @state = State.new(params[:state])
+    if @state.save
+      flash[:notice] = "State has been created."
+      redirect_to admin_states_path
+    else
+      flash[:alert] = "State has not been created."
+      render :action => "new"
+    end
+  end
 end
