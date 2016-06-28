4cd4ab037da699db91028a607ff5889ddcbaa79a
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-06-28 20:25:57 +0200

    marking a state as default

--- spec/integration/managing_states_spec.rb
+++ spec/integration/managing_states_spec.rb
@@ -6,4 +6,15 @@ feature 'Managin states' do
     load Rails.root + "db/seed.rb"
     sign_in_as!(Factory(:admin_user))
   end
+
+  scenario "Marking a state as default" do
+    visit "/"
+    click_link "Admin"
+    click_link "States"
+    within state_line_for("New") do <co id="ch10_740_1"/>
+      click_link "Make Default"
+    end
+
+    page.should have_content("New is now the default state.")
+  end
 end
