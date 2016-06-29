6e2072e2858481b71d37b68aed64d33bd577aa8a
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-06-29 11:33:20 +0200

    default!

--- app/models/state.rb
+++ app/models/state.rb
@@ -4,4 +4,16 @@ class State < ActiveRecord::Base
     name
   end
 
+  def default!
+    current_default_state = State.find_by_default(true)
+    <co id="ch10_813_1"/>
+
+    self.default = true
+    self.save!
+
+    if current_default_state <co id="ch10_813_2"/>
+      current_default_state.default = false
+      current_default_state.save!
+    end
+  end
 end
