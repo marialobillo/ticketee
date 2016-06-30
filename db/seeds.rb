50069fc528f102f70eeb13476a1207d31856828c
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-06-30 08:09:10 +0200

    without protection

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,11 @@ admin_user = User.create(email: "admin@example.com",
                           admin: true)
 Project.create(name: "Ticketee Beta")
 
-State.create(:name => "New",
+State.create({:name => "New",
               :background => "#85FF00",
               :color => "white",
-              :default => true)
+              :default => true},
+              :without_protection => true)
 
 State.create(:name => "Open",
               :background => "#00CFFD",
