057acfda4ba96c7f8f2b10c7305cea3f41a03377
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-07-01 09:22:59 +0200

    assigning permission

--- spec/integration/creating_comments_spec.rb
+++ spec/integration/creating_comments_spec.rb
@@ -52,20 +52,5 @@ feature "Creating comments" do
     <co id="ch10_813_1"/>
   end
 
-  scenario "Changing states for a ticket" do
-    check_permission_box "view", project
-    check_permission_box "change_states", project
-    click_button "Update"
-    click_button "Sign out"
-    sign_in_as!(user)
-    click_link project.name
-    click_link ticket.title
-    fill_in "Text", :with => "Opening this ticket."
-    select "Open", :from => "State"
-    click_button "Create Comment"
-    page.should have_content("Comment has been created.")
-    within("#ticket .state") do
-      page.should have_content("Open")
-    end
-  end
+  
 end
