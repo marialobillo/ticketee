e5de6cdf7619444357dfe6c14bb5293c3722bb3e
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-07-01 09:20:23 +0200

    click_link select ....

--- spec/integration/creating_comments_spec.rb
+++ spec/integration/creating_comments_spec.rb
@@ -57,5 +57,11 @@ feature "Creating comments" do
     check_permission_box "change_states", project
     click_button "Update"
     click_button "Sign out"
+    sign_in_as!(user)
+    click_link project.name
+    click_link ticket.title
+    fill_in "Text", :with => "Opening this ticket."
+    select "Open", :from => "State"
+    click_button "Create Comment"
   end
 end
