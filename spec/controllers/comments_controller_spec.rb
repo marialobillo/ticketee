60683faa8752a5e25fea8349ccab39ac41b90974
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-07-02 13:08:06 +0200

    for comments

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
+  let(:user) { Factory(:confirmed_user) }
+  let(:project) { Project.create(:name => "Ticketee") }
 
 end
